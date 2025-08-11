22685ce7770999f430c6cb60d4e27acb6c5ddae8
dhaneshwar <dhaneshwar6206@gmail.com>
2025-08-11 18:47:36 +0530

    postinstall

--- apps/http-server/src/controllers/userControllers.ts
+++ apps/http-server/src/controllers/userControllers.ts
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { Request,Response } from "express";
 import dotenv from "dotenv";
 import  {prisma} from "@repo/db/client";
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -16,7 +17,8 @@ export const loginController = async (req:Request, res:Response) => {
         },
       });
       if (response) {
-        if (response.password === password) {
+        const result = await bcrypt.compare(password,response.password);
+        if (result) {
           const token = jwt.sign({ id: response.id }, JWT_SECRET as string);
           res.status(200).send({ token, username: response.username });
           return;
@@ -31,25 +33,29 @@ export const loginController = async (req:Request, res:Response) => {
 
 export const signupController = async (req:Request, res:Response) => {
     const { username, firstName, lastName, email, password } = req.body;
-    try {
-      await prisma.user.create({
-        data: {
-          username,
-          firstName,
-          lastName,
-          email,
-          password,
-        },
-      });
-      res.status(200).send({ message: "User created Successfully" });
-    } catch (error: any) {
-      //validating only unique constraints
-      if (error.code === "P2002") {
-        const target = error.meta?.target;
-        //400 Bad Request: The request was not formatted correctly
-        res.status(400).send({ message: `${target} already exists` });
-        return;
-      }
-      res.status(500).send({ message: "Internal Server Error" });
-    }
+    bcrypt.genSalt(8,(err,salt)=>{
+        bcrypt.hash(password,salt,async (err,hash)=>{
+          try {
+            await prisma.user.create({
+              data: {
+                username,
+                firstName,
+                lastName,
+                email,
+                password:hash,
+              },
+            });
+            res.status(200).send({ message: "User created Successfully" });
+          } catch (error: any) {
+            //validating only unique constraints
+            if (error.code === "P2002") {
+              const target = error.meta?.target;
+              //400 Bad Request: The request was not formatted correctly
+              res.status(400).send({ message: `${target} already exists` });
+              return;
+            }
+            res.status(500).send({ message: "Internal Server Error" });
+          }
+        })
+    })
   }
